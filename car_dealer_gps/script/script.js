1a8c099f265cb4bcf958e0cfd578a0e7fc9fa862
KoRrNiK <47156534+KoRrNiK@users.noreply.github.com>
2022-08-26 04:51:23 +0200

    Update gps hack

--- car_dealer_gps/script/script.js
+++ car_dealer_gps/script/script.js
@@ -1,85 +1,93 @@
-const container = document.querySelector('.hackFunction');
-const containerText = document.querySelector('.hackText');
-
+const hackFunction = document.querySelector('.hackFunction');
+const hackFunction2 = document.getElementById('hackFunction2');
+const hackOptions = document.querySelector('.hackOptions');
+const hackText = document.querySelector('.hackText');
 const progressBar = document.getElementById('progressBox');
 const buttonStart = document.getElementById('buttonStart');
 const hackInfo = document.querySelector('.hackInfo');
 const textInfo = document.getElementById('textInfo');
 const progressBarId = document.getElementById('progress-bar');
 
+var __timePlay = document.getElementById('timeChangeInput');
+var __spawnPlay = document.getElementById('spawnChangeInput');
+
 var progressBarInterval;
 var squaresInterval;
 var allSquares;
 
+var defaultTime = 20;
+var defaultSpawn = 350;
+
 const start = () => {
 	allSquares = 0;
 	buttonStart.style.display = 'none';
+	hackOptions.style.display = 'none';
 	progressBar.style.display = 'block';
 	hackInfo.style.display = 'block';
 	textInfo.innerHTML = 'Przygotuj sie...';
 	clearInterval(squaresInterval);
-	progressBarStart('start', 20, 100);
+	progressBarStart('start', 2);
 };
 
 const gameOver = () => {
 	hackInfo.style.display = 'block';
 	textInfo.innerHTML = 'Hack nieudany!';
-	container.style.display = 'none';
-	containerText.style.display = 'none';
-	document.getElementById('hackFunction2').style.display = 'none';
+	hackFunction.style.display = 'none';
+	hackText.style.display = 'none';
+	hackFunction2.style.display = 'none';
 	clearInterval(squaresInterval);
-	progressBarStart('end', 20, 100);
+	progressBarStart('end', 2);
 };
 
-function progressBarStart(type, time, width) {
+hackFunction2.onclick = function () {
+	gameOver();
+};
+
+function progressBarStart(type, time) {
+	var maxwidth = 1000;
+	var width = maxwidth;
 	const process = () => {
 		if (width > 0) {
-			width--;
-			progressBarId.style.width = width + '%';
+			if (type == 'start' || type == 'end') width = width - 3;
+			else width--;
+			progressBarId.style.width = (width * 100.0) / maxwidth + '%';
 		} else {
 			if (type == 'start') {
-				container.style.display = '';
-				containerText.style.display = '';
+				hackFunction.style.display = '';
+				hackText.style.display = '';
 				hackInfo.style.display = 'none';
-				progressBarId.style.width = '100%';
-				progressBarStart('game', 400, 100);
+				progressBarStart('game', __timePlay.value);
 				createNumbers();
 				return;
 			}
 
 			if (type == 'game') {
-				container.style.display = 'none';
-				containerText.style.display = '';
-				progressBarId.style.width = '100%';
-
+				hackFunction.style.display = 'none';
 				hackInfo.style.display = 'block';
 				textInfo.innerHTML = 'Hack Udany';
-				//container.style.opacity = "0";
-				containerText.style.display = 'none';
-
-				document.getElementById('hackFunction2').style.display = 'none';
+				hackText.style.display = 'none';
+				hackFunction2.style.display = 'none';
 				clearInterval(squaresInterval);
-				progressBarStart('end', 20, 100);
+				progressBarStart('end', 2);
 				return;
-			} else if (type == 'end') {
-				container.style.display = 'none';
-				containerText.style.display = 'none';
-				buttonStart.style.display = '';
-				progressBar.style.display = 'none';
-				hackInfo.style.display = 'none';
-				progressBarId.style.width = '100%';
 			}
 
-			clearInterval(progressBarInterval);
+			if (type == 'end') {
+				hackFunction.style.display = 'none';
+				hackText.style.display = 'none';
+				buttonStart.style.display = '';
+				hackOptions.style.display = '';
+				progressBar.style.display = 'none';
+				hackInfo.style.display = 'none';
+			}
 		}
 	};
-
 	clearInterval(progressBarInterval);
 	progressBarInterval = setInterval(process, time);
 }
 
 function createNumbers() {
-	container.innerHTML = '';
+	hackFunction.innerHTML = '';
 
 	let currentIndex = 0;
 
@@ -88,9 +96,9 @@ function createNumbers() {
 		el.classList.add('el');
 		el.setAttribute('id', currentIndex);
 
-		container.appendChild(el);
+		hackFunction.appendChild(el);
 
-		el.onclick = function () {
+		el.onclick /*onmousedown*/ = function () {
 			if (el.classList.contains('select')) {
 				el.classList.remove('select');
 				allSquares--;
@@ -113,17 +121,28 @@ function createNumbers() {
 		}
 
 		if (allSquares > 10) gameOver();
-	}, 350);
+	}, __spawnPlay.value);
 
-	document.getElementById('hackFunction2').style.display = '';
+	hackFunction2.style.display = '';
 }
 
-container.style.display = 'none';
-containerText.style.display = 'none';
+hackFunction.style.display = 'none';
+hackText.style.display = 'none';
 progressBar.style.display = 'none';
 hackInfo.style.display = 'none';
-document.getElementById('hackFunction2').style.display = 'none';
+hackFunction2.style.display = 'none';
 
-document.getElementById('hackFunction2').onclick = function () {
-	gameOver();
-};
+document.getElementById('timeChangeId').innerHTML = String(defaultTime);
+document.getElementById('spawnChangeId').innerHTML = String(defaultSpawn);
+
+function timeChangeFunction() {
+	document.getElementById('timeChangeId').innerHTML =
+		document.getElementById('timeChangeInput').value;
+}
+
+function spawnChangeFunction() {
+	document.getElementById('spawnChangeId').innerHTML =
+		document.getElementById('spawnChangeInput').value;
+}
+
+document.addEventListener('contextmenu', event => event.preventDefault());
